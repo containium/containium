1d0288d1b0dea3076545b4ec67f26cc32d1cf1f4
Arnout Roemers <github@company.romeinszoon.nl>
2013-08-26 16:57:31 +0200

    Optimise routing function for ring modules in Containium with macros.

--- src/containium/http_kit.clj
+++ src/containium/http_kit.clj
@@ -34,36 +34,45 @@
        reverse))
 
 
-(defn- request-matches
-  [{:keys [host-regex context-path] :as ring-conf} request]
-  (and (or (not host-regex)
-           (re-matches host-regex))
-       (or (not context-path)
-           (.startsWith (:uri request) (if (= (last context-path) \/)
-                                         context-path
-                                         (str context-path "/"))))))
-
-
-(defn- handle-request
-  [request app]
-  (when (request-matches (:ring-conf app) request)
-    (boxure/call-in-box (:box app) (:handler-fn app) request)))
+(defmacro matcher
+  [{:keys [host-regex context-path]} uri-sym server-name-sym]
+  (cond (and host-regex context-path) `(and (re-matches ~host-regex ~server-name-sym)
+                                            (.startsWith ~uri-sym ~context-path))
+        host-regex  `(re-matches ~host-regex ~server-name-sym)
+        context-path `(.startsWith ~uri-sym ~context-path)
+        :else true))
 
 
 (defn- make-app
   []
   (let [handler (if-let [apps (seq (vals @apps))]
-                  (let [sorted (sort-apps apps)]
-                    (fn [request] (some (partial handle-request request) sorted)))
+                  (let [sorted (sort-apps apps)
+                        fn-form `(fn [~'request]
+                                   (let [~'uri (:uri ~'request)
+                                         ~'server-name (:server-name ~'request)]
+                                     (or ~@(for [app sorted]
+                                             `(when (matcher ~(:ring-conf app) ~'uri ~'server-name)
+                                                (boxure/call-in-box ~(:box app)
+                                                                    ~(:handler-fn app)
+                                                                    ~'request))))))]
+                    (eval fn-form))
                   (constantly {:status 503 :body "no apps loaded"}))]
     (alter-var-root #'app (constantly handler))))
 
 
 ;;; Ring app registration.
 
+(defn- clean-ring-conf
+  [ring-conf]
+  (if-let [context-path (:context-path ring-conf)]
+    (assoc ring-conf :context-path
+           (if (= (last context-path) \/) context-path (str context-path "/")))
+    ring-conf))
+
+
 (defn upstart-box
   [{:keys [name project] :as box}]
-  (let [ring-conf (-> project :boxure :ring)
+  (let [ring-conf (clean-ring-conf (-> project :boxure :ring))
         handler-fn (boxure/eval box (:handler-sym ring-conf))]
     (swap! apps assoc name (App. box handler-fn ring-conf))
     (make-app)))
