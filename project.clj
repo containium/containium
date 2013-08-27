a6074ec6d6ef9f1d5ea545af728a58c090fb4e3a
Arnout Roemers <github@company.romeinszoon.nl>
2013-08-27 18:06:43 +0200

    Use our patched Clojure explicitly in Containium.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Mozilla Public License 2.0"
             :url "http://mozilla.org/MPL/2.0/"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[boxure/clojure "1.5.1"]
                  [boxure "0.1.0-SNAPSHOT"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [jline "2.11"]
@@ -18,4 +18,5 @@
                  [org.elasticsearch/elasticsearch "0.90.3"]
                  [org.scala-lang/scala-library "2.9.2"]
                  [kafka/core-kafka_2.9.2 "0.7.2"]]
+  :exclusions [org.clojure/clojure]
   :main containium.core)
