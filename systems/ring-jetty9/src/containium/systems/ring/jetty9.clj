8c4e2d9ad78c9720ca240c40cf0271dd4777e60c
Arnout Roemers <github@company.romeinszoon.nl>
2013-12-09 16:27:45 +0100

    Add test Jetty 9 system

--- systems/ring-jetty9/src/containium/systems/ring/jetty9.clj
+++ systems/ring-jetty9/src/containium/systems/ring/jetty9.clj
@@ -10,6 +10,8 @@
             [ring.adapter.jetty9 :as jetty9]))
 
 
+;;; Normal implementation.
+
 (defrecord Jetty9 [server app apps]
   Ring
   (upstart-box [_ name box]
@@ -41,3 +43,33 @@
             server (jetty9/run-jetty app-fn config)]
         (println "Jetty9 server started.")
         (Jetty9. server app (atom {}))))))
+
+
+;;; Test implementation.
+
+(defrecord TestJetty9 [server]
+  Ring
+  (upstart-box [_ _ _]
+    (Exception. "Cannot be used on a test Jetty 9 implementation."))
+  (remove-box [_ _]
+    (Exception. "Cannot be used on a test Jetty 9 implementation."))
+
+  Stoppable
+  (stop [_]
+    (println "Stopping test Jetty 9 server...")
+    (.stop server)
+    (println "Stopped test Jetty 9 server.")))
+
+
+(defn test-jetty9
+  "Create a simple Jetty 9 server, serving the specified ring handler
+  function. This function returns a Startable, requiring a Config
+  system to be available when started."
+  [handler]
+  (reify Startable
+    (start [_ systems]
+      (let [config (get-config (require-system Config systems) :jetty9)
+            _ (println "Starting test Jetty 9 server, using config" config)
+            server (jetty9/run-jetty handler config)]
+        (println "Test Jetty 9 server started.")
+        (TestJetty9. server)))))
